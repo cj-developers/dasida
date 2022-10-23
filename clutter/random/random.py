45d01f5755559043c427a97f3fc4be85ce3e5e79
Woojin Cho <woojin.cho@gsenc.com>
2022-10-23 20:54:36 +0900

    add random, is_test_date

---
+++ clutter/random/random.py
@@ -0,0 +1,14 @@
+import random
+from datetime import datetime
+from typing import Union
+
+
+def is_test_date(_datetime: Union[str, datetime], *, test_ratio=0.1):
+    if not isinstance(_datetime, datetime):
+        _datetime = datetime.fromisoformat(_datetime)
+    _datetime = _datetime.strftime("%Y%m%d%H%M%S")
+    random.seed(_datetime)
+    r = random.random()
+    if r < test_ratio:
+        return True
+    return False
